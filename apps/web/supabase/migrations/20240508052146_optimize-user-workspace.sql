b3c6d1c60d3e5dc0e6df73c8d0703d6fc5bfb57b
giancarlo <giancarlopsk@gmail.com>
2024-05-08 12:22:21 +0700

    Optimize User Workspace by adding indices and triggers
    New SQL migration file has been added which includes an optimization of the User Workspace in the database. This is accomplished by creating indices on the Accounts table, modifying the team_account_workspace function, the user_account_workspace view, and adding new triggers for database operations. These changes provide better performance and effective resource usage.

---
+++ apps/web/supabase/migrations/20240508052146_optimize-user-workspace.sql
@@ -0,0 +1,65 @@
+drop view if exists "public"."user_account_workspace";
+
+CREATE INDEX ix_accounts_is_personal_account ON public.accounts USING btree (is_personal_account);
+
+CREATE INDEX ix_accounts_primary_owner_user_id ON public.accounts USING btree (primary_owner_user_id);
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.team_account_workspace(account_slug text)
+ RETURNS TABLE(id uuid, name character varying, picture_url character varying, slug text, role character varying, role_hierarchy_level integer, primary_owner_user_id uuid, subscription_status subscription_status, permissions app_permissions[])
+ LANGUAGE plpgsql
+AS $function$
+begin
+    return QUERY
+    select
+        accounts.id,
+        accounts.name,
+        accounts.picture_url,
+        accounts.slug,
+        accounts_memberships.account_role,
+        roles.hierarchy_level,
+        accounts.primary_owner_user_id,
+        subscriptions.status,
+        array_agg(role_permissions.permission)
+    from
+        public.accounts
+        inner join public.accounts_memberships
+            on accounts.id = accounts_memberships.account_id
+        left join public.subscriptions
+            on accounts.id = subscriptions.account_id
+        left join public.roles
+            on accounts_memberships.account_role = roles.name
+        left join public.role_permissions
+            on roles.name = role_permissions.role
+    where
+        accounts.slug = account_slug
+        and accounts_memberships.user_id = auth.uid()
+    group by
+        accounts.id,
+        accounts_memberships.account_role,
+        roles.hierarchy_level,
+        subscriptions.status;
+end;
+$function$
+;
+
+create or replace view "public"."user_account_workspace" as  SELECT accounts.id,
+    accounts.name,
+    accounts.picture_url,
+    ( SELECT subscriptions.status
+           FROM subscriptions
+          WHERE (subscriptions.account_id = accounts.id)
+         LIMIT 1) AS subscription_status
+   FROM accounts
+  WHERE ((accounts.primary_owner_user_id = auth.uid()) AND (accounts.is_personal_account = true))
+ LIMIT 1;
+
+
+CREATE TRIGGER accounts_teardown AFTER DELETE ON public.accounts FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
+
+CREATE TRIGGER invitations_insert AFTER INSERT ON public.invitations FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
+
+CREATE TRIGGER subscriptions_delete AFTER DELETE ON public.subscriptions FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
+
+
