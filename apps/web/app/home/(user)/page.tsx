d4654fc6e05ba9b629fafb70350a4e04b009d9a5
SamSantaa <samuel@grayola.io>
2024-07-23 18:47:15 -0500

    General fixces and Home Page

--- apps/web/app/home/(user)/page.tsx
+++ apps/web/app/home/(user)/page.tsx
@@ -1,11 +1,12 @@
 import { PageBody } from '@kit/ui/page';
 import { Trans } from '@kit/ui/trans';
-
 import { createI18nServerInstance } from '~/lib/i18n/i18n.server';
 import { withI18n } from '~/lib/i18n/with-i18n';
 
 import { HomeAccountsList } from './_components/home-accounts-list';
 import { HomeLayoutPageHeader } from './_components/home-page-header';
+import { HomeAccountMetrics } from './_components/home-account-metrics';
+
 
 export const generateMetadata = async () => {
   const i18n = await createI18nServerInstance();
@@ -20,15 +21,17 @@ function UserHomePage() {
   return (
     <>
       <HomeLayoutPageHeader
-        title={<Trans i18nKey={'common:homeTabLabel'} />}
-        description={<Trans i18nKey={'common:homeTabDescription'} />}
+        title="Te damos la bienvenida, Samuel"
+        description=""
       />
 
       <PageBody className={'space-y-4'}>
-        <HomeAccountsList />
+        <HomeAccountMetrics></HomeAccountMetrics>
+
       </PageBody>
     </>
   );
 }
+
 
 export default withI18n(UserHomePage);
