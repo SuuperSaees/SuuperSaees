5cd1e5ea930d25682b76ea8a75690705d426f9d3
JuanGuzman27 <jdg@grayola.io>
2024-08-17 07:46:52 -0500

    Update 20240817124400_updating-database-to-show-files.sql

--- apps/web/supabase/migrations/20240817124400_updating-database-to-show-files.sql
+++ apps/web/supabase/migrations/20240817124400_updating-database-to-show-files.sql
@@ -12,10 +12,4 @@ alter table "public"."order_files" add constraint "order_files_order_id_fkey" FO
 
 alter table "public"."order_files" validate constraint "order_files_order_id_fkey";
 
-CREATE TRIGGER accounts_teardown AFTER DELETE ON public.accounts FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
-
-CREATE TRIGGER invitations_insert AFTER INSERT ON public.invitations FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
-
-CREATE TRIGGER subscriptions_delete AFTER DELETE ON public.subscriptions FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
-
 
