e2067c91c286ba7c31bbf569837fa1c8947036ef
SamSantaa <samuel@grayola.io>
2024-08-21 01:00:56 -0500

    Fix/ Settings page

--- apps/web/app/home/(user)/settings/layout.tsx
+++ apps/web/app/home/(user)/settings/layout.tsx
@@ -8,11 +8,6 @@ import { HomeLayoutPageHeader } from '../_components/home-page-header';
 function UserSettingsLayout(props: React.PropsWithChildren) {
   return (
     <>
-      <HomeLayoutPageHeader
-        title={<Trans i18nKey={'account:accountTabLabel'} />}
-        description={<Trans i18nKey={'account:accountTabDescription'} />}
-      />
-
       {props.children}
     </>
   );
