39768b64aff4574ac4e1affdcaa3b602223e46ea
jairo-holgado-grayola <jairo@grayola.io>
2024-08-30 03:06:46 -0300

    fix:(stripe) get mode product or service

--- apps/web/app/api/stripe/checkout-session/route.ts
+++ apps/web/app/api/stripe/checkout-session/route.ts
@@ -4,13 +4,14 @@ import { NextRequest, NextResponse } from "next/server";
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req: NextRequest) {
-    const { priceId, stripeId } = await req.json(); 
-
-    console.log('Price ID:', priceId);
-    console.log('Stripe ID en la API:', stripeId);
+    const { priceId, stripeId } = await req.json();
 
     try {
-    
+
+        // HERE 
+        const price = await stripe.prices.retrieve(priceId);
+        const {type} = price;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'], 
             line_items: [
@@ -19,17 +20,13 @@ export async function POST(req: NextRequest) {
                     quantity: 1, 
                 },
             ],
-            mode: 'payment', // It works if it is a one-time payment
-            // mode: 'subscription', // It works if it is a subscription type payment
+            mode: type === "recurring" ? "subscription" : "payment",
             success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/success?session_id={CHECKOUT_SESSION_ID}`, 
             cancel_url: `${process.env.NEXT_PUBLIC_SITE_URL}/cancel`,
         }, {
             stripeAccount: stripeId,
         }
     );
-
-        console.log('Session ID:', session);
-
         return NextResponse.json({ sessionUrl: session.url });
     } catch (error) {
         console.error('Error creating checkout session:', error);
