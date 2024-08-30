07762ea4a471c87b137be5ffe77ff6b51b799e37
jairo-holgado-grayola <jairo@grayola.io>
2024-08-29 23:36:15 -0300

    fix:(database) fix on migrate process and reset database, supabase

--- apps/web/supabase/migrations/20240827213817_policy-changes-for-permissions.sql
+++ apps/web/supabase/migrations/20240827213817_policy-changes-for-permissions.sql
@@ -1,14 +1,19 @@
-drop policy "Delete for all authenticated users" on "public"."messages";
+alter type public.app_permissions add value 'messages.write';
+-- alter type public.app_permissions add value 'messages.delete';
+alter type public.app_permissions add value 'messages.read';
+commit;
 
-drop policy "Read for all authenticated users" on "public"."messages";
+-- drop policy "Delete for all authenticated users" on "public"."messages";
 
-alter type "public"."app_permissions" rename to "app_permissions__old_version_to_be_dropped";
+-- drop policy "Read for all authenticated users" on "public"."messages";
 
-create type "public"."app_permissions" as enum ('roles.manage', 'billing.manage', 'settings.manage', 'members.manage', 'invites.manage', 'tasks.write', 'tasks.delete', 'messages.write', 'messages.read');
+-- alter type "public"."app_permissions" rename to "app_permissions__old_version_to_be_dropped";
 
-alter table "public"."role_permissions" alter column permission type "public"."app_permissions" using permission::text::"public"."app_permissions";
+-- create type "public"."app_permissions" as enum ('roles.manage', 'billing.manage', 'settings.manage', 'members.manage', 'invites.manage', 'tasks.write', 'tasks.delete', 'messages.write', 'messages.read');
 
-drop type "public"."app_permissions__old_version_to_be_dropped";
+-- alter table "public"."role_permissions" alter column permission type "public"."app_permissions" using permission::text::"public"."app_permissions";
+
+-- drop type "public"."app_permissions__old_version_to_be_dropped";
 
 create policy "Enable delete for users based on user_id"
 on "public"."messages"
