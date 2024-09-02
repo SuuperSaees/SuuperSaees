92402733128f9928bdfc2b0ad00bd3e9dfc0c848
jairo-holgado-grayola <jairo@grayola.io>
2024-09-02 16:08:18 -0300

    fix:(migration file) file with new migration config

--- apps/web/supabase/migrations/20240902184405_create-trigger-invitations.sql
+++ apps/web/supabase/migrations/20240902184405_create-trigger-invitations.sql
@@ -5,5 +5,3 @@ CREATE TRIGGER accounts_teardown AFTER DELETE ON public.accounts FOR EACH ROW EX
 -- CREATE TRIGGER invitations_insert AFTER INSERT ON public.invitations FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
 
 CREATE TRIGGER subscriptions_delete AFTER DELETE ON public.subscriptions FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
-
-
