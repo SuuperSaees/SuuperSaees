ccc6aaa44419cda9727dd85290985d5781083c6b
juan-roman-grayola <juan.garzon@grayola.io>
2024-09-03 09:25:43 -0500

    hotfix(migration-files): commenting unnecessary creation of triggers

--- apps/web/supabase/migrations/20240902190455_add-trigger-invitations-insert.sql
+++ apps/web/supabase/migrations/20240902190455_add-trigger-invitations-insert.sql
@@ -1,4 +1,4 @@
-CREATE TRIGGER invitations_insert AFTER INSERT ON public.invitations FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
+-- CREATE TRIGGER invitations_insert AFTER INSERT ON public.invitations FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('http://host.docker.internal:3000/api/db/webhook', 'POST', '{"Content-Type":"application/json", "X-Supabase-Event-Signature":"WEBHOOKSECRET"}', '{}', '5000');
 
 alter table "public"."accounts" alter column "stripe_id" drop default;
 
