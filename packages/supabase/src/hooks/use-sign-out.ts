f939f09cf7b7c3785e6230655e6e1bc676f49f92
Juan Guzman <juan.guzman@suuper.co>
2024-09-18 14:57:10 -0500

    Redirect page is now fetched from environment variables

--- packages/supabase/src/hooks/use-sign-out.ts
+++ packages/supabase/src/hooks/use-sign-out.ts
@@ -2,14 +2,19 @@ import { useMutation } from '@tanstack/react-query';
 
 import { useSupabase } from './use-supabase';
 
+if (!process.env.NEXT_PUBLIC_SITE_URL) {
+  throw new Error("Redirect URL is not in environment variables");
+}
+
+const landing_page = process.env.NEXT_PUBLIC_SITE_URL;
+
 export function useSignOut() {
   const client = useSupabase();
 
   return useMutation({
     mutationFn: async () => {
       await client.auth.signOut();
-      // Redirige después de cerrar sesión
-      window.location.href = 'https://suuper.co/';
+      window.location.href = landing_page;
     },
     onError: (error) => {
       console.error('Error al cerrar sesión:', error.message);
