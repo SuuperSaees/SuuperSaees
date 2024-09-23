a32468bf537816ab3177c54a17e52c1701060195
jairo-holgado-suuper <jairo.holgado@suuper.co>
2024-09-23 11:37:27 -0300

    fis(signOut, confirmEmail and Components): correct version

--- packages/supabase/src/hooks/use-sign-out.ts
+++ packages/supabase/src/hooks/use-sign-out.ts
@@ -2,12 +2,22 @@ import { useMutation } from '@tanstack/react-query';
 
 import { useSupabase } from './use-supabase';
 
+if (!process.env.NEXT_PUBLIC_LANDING_URL) {
+  throw new Error("Redirect URL is not in environment variables");
+}
+
+const landing_page = process.env.NEXT_PUBLIC_LANDING_URL;
+
 export function useSignOut() {
   const client = useSupabase();
 
   return useMutation({
-    mutationFn: () => {
-      return client.auth.signOut();
+    mutationFn: async () => {
+      await client.auth.signOut();
+      window.location.href = landing_page;
     },
+    onError: (error) => {
+      console.error('Error al cerrar sesión:', error.message);
+    }
   });
 }
