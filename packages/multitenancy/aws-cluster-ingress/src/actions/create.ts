06d27a32a6b634e0966230eb25a3745ee396c16c
jairo-holgado-suuper <jairo.holgado@suuper.co>
2024-10-08 15:14:58 -0300

    feat(multitenancy) create ingress front implementation function

---
+++ packages/multitenancy/aws-cluster-ingress/src/actions/create.ts
@@ -0,0 +1,25 @@
+export async function createIngress({
+  domain,
+}: {
+  domain: string;
+}): Promise<any> {
+  const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL;
+  const newDomainCreate = {
+    domain: `${domain}.suuper.co`,
+    namespace: 'prod',
+    service_name: 'ms-suuper-prod',
+  };
+  const response = await fetch(`${BASE_URL}/api/v1/multitenancy/subdomains`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newDomainCreate),
+  });
+  if (!response.ok) {
+    throw new Error('Failed to create ingress');
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
