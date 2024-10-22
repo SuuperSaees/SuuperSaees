398b733a1a1ba2caafbe88de01ef7d7783df44f3
Juan Guzman <juan.guzman@suuper.co>
2024-10-21 22:00:01 -0500

    fix (files and storage settings): adding the storage translations and adding a relation when is getting the files in a folder

--- apps/web/components/organization/files/option-files.tsx
+++ apps/web/components/organization/files/option-files.tsx
@@ -30,13 +30,7 @@ import {
   DropdownMenuTrigger,
 } from '@kit/ui/dropdown-menu';
 
-function generateUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0,
-      v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
+import { generateUUID } from '~/utils/generate-uuid';
 
 export function OptionFiles({
   clientOrganizationId,
