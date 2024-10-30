32ac9425c491493b23c97222e283cab5b5f73957
dsolisav <dsolis@unal.edu.co>
2024-10-30 11:10:43 -0500

    feat(orders): Add delete option and server action to orders

---
+++ apps/web/app/orders/[id]/components/delete-order-dropdown.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+
+import { EllipsisVertical, Trash2 } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
+import { deleteOrderByUuid } from '~/team-accounts/src/server/actions/orders/delete/delete-order';
+import {toast} from 'sonner';
+
+import { Button } from '@kit/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@kit/ui/dialog';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@kit/ui/dropdown-menu';
+import { useRouter } from 'next/navigation';
+
+function DeleteOrderDropdown({orderUuid}: {orderUuid: string}) {
+  const { t } = useTranslation('orders');
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const router = useRouter()
+
+  async function handleDelete() {
+    try {
+      await deleteOrderByUuid(orderUuid);
+      setIsDialogOpen(false);
+      router.push('/orders');
+      router.refresh();
+      toast.success(t('deleteSuccess'));
+    } catch (error) {
+      console.error('Error deleting the order:', error);
+      setIsDialogOpen(false);
+      toast.success(t('deleteError'));
+    }
+  }
+
+  return (
+    <div className="mb-[0.20rem] ml-2">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <EllipsisVertical className="h-5 w-5" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            onSelect={(event) => {
+              event.preventDefault();
+              setIsDialogOpen(true);
+            }}
+          >
+            <div className="flex items-center gap-2 text-gray-800">
+              <Trash2 className="h-4 w-4" />
+              <p>{t('delete')}</p>
+            </div>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{t('deleteConfirmation')}</DialogTitle>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setIsDialogOpen(false)}>
+              {t('cancel')}
+            </Button>
+            <Button variant="destructive" onClick={handleDelete}>
+              {t('delete')}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+export default DeleteOrderDropdown;
