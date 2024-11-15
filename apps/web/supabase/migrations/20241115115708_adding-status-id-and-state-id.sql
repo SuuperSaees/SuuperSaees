c7d22d6c8e8694456e0a6edd9a42f925d2732fd0
jairo-holgado-suuper <jairo.holgado@suuper.co>
2024-11-15 09:05:50 -0300

    feat(migrations) adding status id and state id

---
+++ apps/web/supabase/migrations/20241115115708_adding-status-id-and-state-id.sql
@@ -0,0 +1,11 @@
+alter table "public"."orders_v2" add column "status_id" bigint;
+
+alter table "public"."subtasks" add column "state_id" bigint;
+
+alter table "public"."orders_v2" add constraint "orders_v2_status_id_fkey" FOREIGN KEY (status_id) REFERENCES agency_statuses(id) ON DELETE SET NULL not valid;
+
+alter table "public"."orders_v2" validate constraint "orders_v2_status_id_fkey";
+
+alter table "public"."subtasks" add constraint "subtasks_state_id_fkey" FOREIGN KEY (state_id) REFERENCES agency_statuses(id) not valid;
+
+alter table "public"."subtasks" validate constraint "subtasks_state_id_fkey";
