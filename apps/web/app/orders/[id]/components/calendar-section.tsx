e8bc647fa6984ff24a8fd9614e543d7f1b000604
dsolisav <dsolis@unal.edu.co>
2024-11-18 18:28:44 -0500

    fix(calendars): Fix error 500

--- apps/web/app/orders/[id]/components/calendar-section.tsx
+++ apps/web/app/orders/[id]/components/calendar-section.tsx
@@ -32,7 +32,7 @@ function CalendarSection({
       <p className="mb-2 font-bold">{t('calendarTitle')}</p>
       {loading ? (
         <Spinner className="h-7 w-7" />
-      ) : orderAgencyMembers.every((member) => !member.user_settings.calendar) ? (
+      ) : orderAgencyMembers.every((member) => !member.user_settings?.calendar) ? (
         <p>{t('calendarEmpty')}</p>
       ) : (
         <Tabs
@@ -44,7 +44,7 @@ function CalendarSection({
         >
           <TabsList className="flex w-fit gap-2 bg-transparent">
             {orderAgencyMembers?.map((member) =>
-              member.user_settings.calendar ? (
+              member.user_settings?.calendar ? (
                 <ThemedTabTrigger
                   className="flex items-center rounded-full p-0"
                   key={member.id}
@@ -53,7 +53,7 @@ function CalendarSection({
                   option={member.id}
                 >
                   <Avatar className="scale-50 p-0">
-                    <AvatarImage src={member.user_settings.picture_url ?? ''} />
+                    <AvatarImage src={member.user_settings?.picture_url ?? ''} />
                     <AvatarFallback>
                       {member.name.charAt(0).toUpperCase() || 'N/A'}
                     </AvatarFallback>
@@ -66,13 +66,13 @@ function CalendarSection({
             )}
           </TabsList>
           {orderAgencyMembers?.map((member) =>
-            member.user_settings.calendar ? (
+            member.user_settings?.calendar ? (
               <TabsContent key={member.id} value={member.id}>
                 <div className="max-h-screen w-full">
                   <div className="aspect-[4/3] h-full max-h-96 w-full overflow-auto lg:max-h-[30rem]">
                     <iframe
                       className="h-full w-full overflow-scroll"
-                      src={member.user_settings.calendar ?? ''}
+                      src={member.user_settings?.calendar ?? ''}
                     ></iframe>
                   </div>
                 </div>
