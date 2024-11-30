2faed47b283e2b4456750db5537b6fc1fc1796ab
juandgr-suuper <juan.garzon@suuper.co>
2024-11-29 21:31:55 -0500

    fix(orders-review-card): adjust style

--- apps/web/app/orders/[id]/components/user-review-message.tsx
+++ apps/web/app/orders/[id]/components/user-review-message.tsx
@@ -10,23 +10,23 @@ interface UserReviewMessageProps {
 }
 const UserReviewMessage = ({ review }: UserReviewMessageProps) => {
   return (
-    <div className="flex gap-2">
-      <div className="flex h-10 w-10 items-center justify-center rounded-full bg-yellow-100">
+    <div className="flex gap-2 w-full">
+      <div className="flex h-10 w-10 items-center justify-center rounded-full bg-yellow-100 ">
         <Star className="h-5 w-5 text-yellow-400" />
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col w-full">
         <div className="flex justify-between gap-4">
           <p className="font-semibold text-gray-600">{`Left a ${review?.rating} star review:`}</p>
           <small className="">
             {format(new Date(review?.created_at), 'MMM dd, p')}
           </small>
         </div>
-        <div className="flex gap-1 rounded-lg rounded-ss-none bg-gray-50 p-3">
-          <AvatarDisplayer
-            pictureUrl={review?.user?.picture_url}
-            displayName={null}
-            text={review?.user.name ? review.user.name : undefined}
-          />
+        <div className="flex gap-2 rounded-lg rounded-ss-none bg-gray-50 p-3">
+        <AvatarDisplayer
+        displayName={review?.user.picture_url ? null : review?.user.name}
+        pictureUrl={review?.user.picture_url}
+        text={review?.user.name ? review.user.name : undefined}
+      />
           <div className="flex flex-col gap-1">
             <span className="font-semibold">{review?.user?.name}</span>
             <p>{review?.content}</p>
