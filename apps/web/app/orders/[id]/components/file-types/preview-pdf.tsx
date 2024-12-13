299f08fb0e860007595f0e9a918def42e661b59d
dsolisav <dsolis@unal.edu.co>
2024-12-13 07:49:57 -0500

    hotfix(details): Add file action buttons in details tab

--- apps/web/app/orders/[id]/components/file-types/preview-pdf.tsx
+++ apps/web/app/orders/[id]/components/file-types/preview-pdf.tsx
@@ -1,4 +1,4 @@
-const PreviewPDF = ({ url }) => (
+const PreviewPDF = ({ url }: { url: string }) => (
     <iframe className='w-[192px] h-[137px] rounded-lg' src={url} type='application/pdf'>
       Your browser does not support PDFs.
     </iframe>
