a6dbde43111f817cc7a1cbc2c83742badad47267
Martin <martinalturria@hotmail.com>
2025-01-26 12:03:17 -0300

    fix(plugins): button stripe reconnect and connect

--- apps/web/app/apps/components/stripe-content.tsx
+++ apps/web/app/apps/components/stripe-content.tsx
@@ -1,10 +1,49 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-function StripeContent() {
+import { getAccountPluginByIdAction } from '../../../../../packages/plugins/src/server/actions/account-plugins/get-account-plugin-by-Id';
+
+function StripeContent({
+  pluginId,
+}: {
+  pluginId: string;
+  userId: string;
+}) {
   const router = useRouter();
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!pluginId) return;
+
+    const fetchPluginData = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await getAccountPluginByIdAction(pluginId);
+        console.log("RESPUESTAAAAA", response);
+
+        if (response?.success) {
+          const providerId = response.success.data?.provider_id;
+          console.log("RESPONESSSSSSSSS", providerId);
+
+          setIsConnected(!!providerId);
+        } else {
+          console.error('Error fetching plugin:', response?.error?.message);
+        }
+      } catch (error) {
+        console.error('Error fetching plugin data:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    void fetchPluginData();
+  }, [pluginId]);
 
   const handleConnect = () => {
     router.push('/stripe');
@@ -26,8 +65,9 @@ function StripeContent() {
       <button
         onClick={handleConnect}
         className="text-sm font-medium text-blue-600 hover:underline"
+        disabled={isLoading}
       >
-        Conectar
+        {isLoading ? 'Loading...' : isConnected ? 'Reconnect' : 'Connect'}
       </button>
     </div>
   );
