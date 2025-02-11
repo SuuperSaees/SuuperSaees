caf1ffef36a7542422ee87b3287a6cf188757985
juandgr-suuper <juan.garzon@suuper.co>
2025-02-11 10:45:38 -0500

    fix(chat-context): add order_id filter for exclude messages from other chat

--- apps/web/app/messages/components/context/chat-context.tsx
+++ apps/web/app/messages/components/context/chat-context.tsx
@@ -126,6 +126,9 @@ export function ChatProvider({
       setData: setMessages as Dispatch<
         SetStateAction<Message.Type[] | Message.Type>
       >,
+      filter: {
+        order_id: 'null'
+      },
     },
     // {
     //   tableName: 'chats',
