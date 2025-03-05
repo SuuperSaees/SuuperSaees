ca9e7f007bf57ca635236f2fdb442d20b869029d
jairo-holgado-suuper <jairo.holgado@suuper.co>
2025-03-05 17:39:50 -0300

    fix(embeds): list embeds

--- apps/web/app/server/actions/embeds/repositories/embeds.repository.ts
+++ apps/web/app/server/actions/embeds/repositories/embeds.repository.ts
@@ -65,15 +65,49 @@ export class EmbedsRepository {
         };
     }
 
-    async list(organizationId?: string, role?: string): Promise<Embeds.Type[]> {
+    async list(organizationId?: string, role?: string, agencyId?: string): Promise<Embeds.TypeWithRelations[]> {
+        const agencyRoles = new Set(['agency_owner', 'agency_member', 'agency_project_manager']);
+        const clientRoles = new Set(['client_owner', 'client_member', 'client_guest']);
+
+        const isClient = clientRoles.has(role ?? '');
+        if(isClient && !agencyId) {
+            throw new Error('Agency ID is required for client role');
+        }
+
+        const isAgency = agencyRoles.has(role ?? '');
+
+        if(isAgency && !organizationId) {
+            throw new Error('Organization ID is required for agency role');
+        }
+
+        if(isClient){
+            const { data: embedData, error: embedError } = await this.client
+            .from('embeds')
+            .select(`
+                *,
+                embed_accounts!inner(*)
+            `)
+            .eq('organization_id', agencyId ?? '')
+            .is('deleted_on', null)
+            .or(`visibility.neq.private, and(visibility.eq.private, embed_accounts.account_id.eq.${organizationId})`)
+
+            if (embedError && embedError.code !== 'PGRST116') throw embedError;
+
+            const uniqueEmbeds = Array.from(
+                new Map(embedData?.map(embed => [embed.id, embed])).values()
+            );
+
+            return uniqueEmbeds ?? [];
+        }
+
         const { data: embedData, error: embedError } = await this.client
         .from('embeds')
         .select('*')
         .eq('organization_id', organizationId ?? '')
         .is('deleted_on', null);
 
-        if (embedError) throw embedError;
+        if (embedError && embedError.code !== 'PGRST116') throw embedError;
 
-        return embedData;
+        return embedData ?? [];
     }
 }
