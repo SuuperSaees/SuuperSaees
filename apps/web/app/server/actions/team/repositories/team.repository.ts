eee00aa909908d57d0e2e091818725da5bb078d0
juandgr-suuper <juan.garzon@suuper.co>
2025-04-30 15:20:57 -0500

    fix: refine member selection logic in ChatMembersSelector and TeamRepository
    - Updated ChatMembersSelector to ensure only active agency and client members are included in the selection.
    - Enhanced TeamRepository to filter out inactive members during retrieval, improving data integrity.

--- apps/web/app/server/actions/team/repositories/team.repository.ts
+++ apps/web/app/server/actions/team/repositories/team.repository.ts
@@ -112,17 +112,18 @@ export class TeamRepository {
         if (membersDataError) throw membersDataError;
 
         const membersDataMap = new Map(membersData.map((member) => [member.id, member]));
-        
-        resultMembers[organizationId].members = members.map((member) => ({
-          id: member.user_id,
-          name: membersDataMap.get(member.user_id)?.user_settings?.[0]?.name ?? membersDataMap.get(member.user_id)?.name ??  '',
-          email: membersDataMap.get(member.user_id)?.email ?? '',
-          organization_id: organizationId,
-          role: member.account_role,
-          picture_url: membersDataMap.get(member.user_id)?.user_settings?.[0]?.picture_url ?? membersDataMap.get(member.user_id)?.picture_url ??  '',
-          // visibility: true,
-        }
-      ));
+        resultMembers[organizationId].members = members
+          .filter((member) => membersDataMap.has(member.user_id))
+          .map((member) => ({
+            id: member.user_id,
+            name: membersDataMap.get(member.user_id)?.user_settings?.[0]?.name ?? membersDataMap.get(member.user_id)?.name ??  '',
+            email: membersDataMap.get(member.user_id)?.email ?? '',
+            organization_id: organizationId,
+            role: member.account_role,
+            picture_url: membersDataMap.get(member.user_id)?.user_settings?.[0]?.picture_url ?? membersDataMap.get(member.user_id)?.picture_url ??  '',
+            // visibility: true,
+          }));
+
       }
       
 
