f6b361f3572c61320c01e31aedb8bdaf448a447e
jairo-holgado-suuper <jairo.holgado@suuper.co>
2025-05-08 07:59:22 -0300

    refactor: enhance session management in accounts action
    - Introduced cookie handling to retrieve session information from cookies.
    - Improved session validation by checking expiration and parsing session data.
    - Refactored getSessionAction and getSession functions for better clarity and maintainability.

--- apps/web/app/server/actions/accounts/accounts.action.ts
+++ apps/web/app/server/actions/accounts/accounts.action.ts
@@ -1,15 +1,34 @@
 'use server'
 
 import { createAccountsAction } from "./accounts";
-import {cache } from 'react'
+import { cookies } from 'next/headers';
+import { Session } from "~/lib/account.types";
 
 function getAccountsAction(host?: string) {
     return createAccountsAction(host ?? process.env.NEXT_PUBLIC_SITE_URL as string);
 }
 
 export async function getSessionAction() {
+    const cookieStore = cookies();
+    const sessionCookie = cookieStore.get('session_info');
+    
+    if (sessionCookie) {
+        try {
+            const sessionData = JSON.parse(sessionCookie.value);
+            if (sessionCookie.expires && new Date(sessionCookie.expires) > new Date()) {
+                return sessionData as Session.Type;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
     const accountsAction = getAccountsAction();
-    return await accountsAction.getSession();
+    const session = await accountsAction.getSession();
+    
+    return session;
 }
 
-export const getSession = cache(getSessionAction);
+export async function getSession() {
+    return await getSessionAction();
+}
